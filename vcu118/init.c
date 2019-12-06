24bc8369b096848a5207fe9b261bd1f9941ee0a5
John Merrill <jmerrill@draper.com>
2019-12-06 10:58:25 -0500

    Send UART message when main() completes

--- vcu118/init.c
+++ vcu118/init.c
@@ -4,8 +4,14 @@ extern int main(void);
 
 void _init(void)
 {
+  int result;
+
   uart0_init();
 
-  main();
+  result = main();
+
+  // isp_run_app should exit upon seeing this message
+  printf("Program exited with code: %d\n", result);
+
   for(;;);
 }
