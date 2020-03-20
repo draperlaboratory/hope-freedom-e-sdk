00c6421041867b961a60b6a2a81049637b1aa518
Alec Roelke <aroelke@draper.com>
2020-03-20 13:10:28 -0400

    Fix xilinx types to be the correct width on 64-bit

--- vcu118/xilinx/common/xbasic_types.h
+++ vcu118/xilinx/common/xbasic_types.h
@@ -105,19 +105,20 @@ extern void XAssert(char *, int);
 
 /**************************** Type Definitions *******************************/
 
+#include <stdint.h>
 /** @name Legacy types
  * Deprecated legacy types.
  * @{
  */
-typedef unsigned char	Xuint8;		/**< unsigned 8-bit */
-typedef char		Xint8;		/**< signed 8-bit */
-typedef unsigned short	Xuint16;	/**< unsigned 16-bit */
-typedef short		Xint16;		/**< signed 16-bit */
-typedef unsigned long	Xuint32;	/**< unsigned 32-bit */
-typedef long		Xint32;		/**< signed 32-bit */
-typedef float		Xfloat32;	/**< 32-bit floating point */
-typedef double		Xfloat64;	/**< 64-bit double precision FP */
-typedef unsigned long	Xboolean;	/**< boolean (XTRUE or XFALSE) */
+typedef uint8_t     Xuint8;		/**< unsigned 8-bit */
+typedef int8_t      Xint8;		/**< signed 8-bit */
+typedef uint16_t    Xuint16;	/**< unsigned 16-bit */
+typedef int16_t     Xint16;		/**< signed 16-bit */
+typedef uint32_t    Xuint32;	/**< unsigned 32-bit */
+typedef int32_t     Xint32;		/**< signed 32-bit */
+typedef float       Xfloat32;	/**< 32-bit floating point */
+typedef double      Xfloat64;	/**< 64-bit double precision FP */
+typedef unsigned long   Xboolean;	/**< boolean (XTRUE or XFALSE) */
 
 #if !defined __XUINT64__
 typedef struct
