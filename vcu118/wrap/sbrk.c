33cab29eeaf05727e93a3e8634e3b7638da6bdfb
Alec Roelke <aroelke@draper.com>
2020-08-05 09:27:09 -0400

    Treat AP mailbox like an array

--- vcu118/wrap/sbrk.c
+++ vcu118/wrap/sbrk.c
@@ -6,10 +6,7 @@ extern uintptr_t _heap_end;
 static void* program_break = &_heap_end;
 static const volatile uintptr_t sbrk_zero = 0;
 
-uint32_t* const AP_MB0 = (uint32_t*)0x10090U;
-uint32_t* const AP_MB1 = (uint32_t*)0x10094U;
-uint32_t* const AP_MB2 = (uint32_t*)0x10098U;
-uint32_t* const AP_MB3 = (uint32_t*)0x1009cU;
+uint32_t* const AP_MB = (uint32_t*)0x10090U;
 
 void* __wrap_sbrk(intptr_t increment)
 {
@@ -17,16 +14,16 @@ void* __wrap_sbrk(intptr_t increment)
   if (increment == 0)
     return program_break;
 
-  *AP_MB0 = 1;
+  AP_MB[0] = 1;
 
   // Ensure the new program break always aligns to pointer size
   increment = (increment + sizeof(uintptr_t) - 1)/sizeof(uintptr_t)*sizeof(uintptr_t);
 
   // Move the fake program break
-  *AP_MB1 = (uint32_t)program_break;
+  AP_MB[1] = (uint32_t)program_break;
   void* temp = program_break;
   program_break += increment;
-  *AP_MB2 = (uint32_t)program_break;
+  AP_MB[2] = (uint32_t)program_break;
 
   // Update tags for newly-allocated or deallocated memory
   uintptr_t size = increment > 0 ? increment : -increment;
