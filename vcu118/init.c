4ec0a2241e1c9420a065672a9b38aa4a21c0cf25
Silviu Chiricescu <silviu@draper.com>
2021-05-04 09:26:43 -0400

    * Enable interrupts, set up the mtvec, and print a simple  debug msg when a trap occurs so we know the mtval, mepc, and mcause.

--- vcu118/init.c
+++ vcu118/init.c
@@ -3,12 +3,30 @@
 
 extern int main(void);
 
+#define write_csr(reg, val) \
+  asm volatile ("csrw " #reg ", %0" :: "r"(val))
+
+#define read_csr(reg) ({ unsigned long __tmp;               \
+            asm volatile ("csrr %0, " #reg : "=r"(__tmp));  \
+            __tmp; })
+
+void bad_trap(){
+  unsigned long mepc, mtval, mcause;
+  asm volatile("csrr %0, mcause" : "=r"(mcause));
+  asm volatile("csrr %0, mtval" : "=r"(mtval));
+  asm volatile("csrr %0, mepc" : "=r"(mepc));
+  printf("\n FAILURE: \n mcause=0x%lx\n, mepc=0x%lx \n, mtval=0x%lx \n", mcause, mepc, mtval);
+}
+
 void _init(void)
 {
   int result;
 
   uart0_init();
 
+   // set up a bad trap handler
+  write_csr(mtvec, ((uintptr_t)&bad_trap));
+  
   // set up floating point computation
   uintptr_t misa, mstatus;
   asm volatile("csrr %0, misa" : "=r"(misa));
