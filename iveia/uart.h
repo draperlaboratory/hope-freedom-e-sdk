2648397fdf72388c494e2d8d778512cb693179d6
Silviu Chiricescu <silviu@draper.com>
2021-07-14 14:34:43 -0400

    * Use uint8_t instead of char.

--- iveia/uart.h
+++ iveia/uart.h
@@ -22,54 +22,54 @@
  * Receive a single byte. Polling mode, waits until finished
  */
 #define UART_RXCHAR(uart_number) \
-  char uart ## uart_number ## _rxchar(void) \
-  { return (char)uart_rxchar(&Uart##uart_number);}
+  uint8_t uart ## uart_number ## _rxchar(void) \
+  { return uart_rxchar(&Uart##uart_number);}
 
 /**
  * Transmit a single byte. Polling mode, waits until finished
  */
 #define UART_TXCHAR(uart_number) \
-  char uart##uart_number##_txchar(char c) \
-  { return (char)uart_txchar(&Uart##uart_number, (uint8_t)c); }
+  uint8_t uart##uart_number##_txchar(uint8_t c) \
+  { return uart_txchar(&Uart##uart_number, c); }
 /**
  * Transmit a buffer. Waits indefinitely for a UART TX mutex,
  * returns number of transferred bytes or -1 in case of an error.
  * Synchronous API.
  */
 #define UART_TXBUFFER(uart_number) \
-  int uart##uart_number##_txbuffer(char *ptr, int len) \
-  { return uart_txbuffer(&Uart##uart_number, (uint8_t *)ptr, len); }
+  int uart##uart_number##_txbuffer(uint8_t *ptr, int len) \
+  { return uart_txbuffer(&Uart##uart_number, ptr, len); }
 
 
 #define UART_RXBUFFER(uart_number) \
-  int uart##uart_number##_rxbuffer(char *ptr, int len) \
-  { return uart_rxbuffer(&Uart##uart_number, (uint8_t *)ptr, len);}
+  int uart##uart_number##_rxbuffer(uint8_t *ptr, int len) \
+  { return uart_rxbuffer(&Uart##uart_number, ptr, len);}
 
 
 #if BSP_USE_UART0
 bool uart0_rxready(void);
-char uart0_rxchar(void);
-char uart0_txchar(char c);
-int uart0_rxbuffer(char *ptr, int len);
-int uart0_txbuffer(char *ptr, int len);
+uint8_t uart0_rxchar(void);
+uint8_t uart0_txchar(uint8_t c);
+int uart0_rxbuffer(uint8_t *ptr, int len);
+int uart0_txbuffer(uint8_t *ptr, int len);
 void uart0_init(void);
 #endif
 
 #if BSP_USE_UART1
 bool uart1_rxready(void);
-char uart1_rxchar(void);
-char uart1_txchar(char c);
-int uart1_rxbuffer(char *ptr, int len);
-int uart1_txbuffer(char *ptr, int len);
+uint8_t uart1_rxuint8_t(void);
+uint8_t uart1_txuint8_t(uint8_t c);
+int uart1_rxbuffer(uint8_t *ptr, int len);
+int uart1_txbuffer(uint8_t *ptr, int len);
 void uart1_init(void);
 #endif
 
 #if BSP_USE_UART2
 bool uart2_rxready(void);
-char uart2_rxchar(void);
-char uart2_txchar(char c);
-int uart2_rxbuffer(char *ptr, int len);
-int uart2_txbuffer(char *ptr, int len);
+uint8_t uart2_rxuint8_t(void);
+uint8_t uart2_txuint8_t(uint8_t c);
+int uart2_rxbuffer(uint8_t *ptr, int len);
+int uart2_txbuffer(uint8_t *ptr, int len);
 void uart2_init(void);
 #endif
 
